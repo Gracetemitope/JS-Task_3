d39d56c421c3ae76ce3d166c45879ffc6cbf7afa
Gracetemitope <58818795+Gracetemitope@users.noreply.github.com>
2020-04-19 15:37:24 +0100

    task completed

---
+++ solution.js
@@ -0,0 +1,25 @@
+function solution(num) {
+  let arr = [];
+  for(let i = 1; i<= num; i++) {
+    if(i % 2 === 0 && i % 3 === 0 && i % 5 === 0){
+      arr.push("yu-gi-oh");
+    } else if(i % 2 === 0 && i % 3 === 0) {
+      arr.push("yu-gi");
+    } else if(i % 2 === 0 && i % 5 === 0) {
+      arr.push("yu-oh");
+    } else if(i % 3 === 0 && i % 5 === 0) {
+      arr.push("gi-oh");
+    } else if(i % 2 === 0) {
+      arr.push("yu");
+    } else if(i % 3 === 0) {
+      arr.push("gi");
+    } else if(i %  5 === 0) {
+      arr.push("oh");
+    } else {
+      arr.push(i)
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+solution(300);
